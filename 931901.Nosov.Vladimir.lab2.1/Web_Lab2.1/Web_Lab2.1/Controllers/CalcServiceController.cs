680091b26fb0ef7f4e5af47cf05761bc395c7a3b
Boobenchick <vova.nosov@bk.ru>
2022-03-21 09:42:26 +0700

    1

---
+++ 931901.Nosov.Vladimir.lab2.1/Web_Lab2.1/Web_Lab2.1/Controllers/CalcServiceController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLab2._1.Models;
+using WebLab2._1.Services;
+
+namespace WebLab2._1.Controllers
+{
+    [Route("CalcService")]
+    public class CalcServiceController : Controller
+    {
+        [Route("PassUsingModel")]
+        public IActionResult PassUsingModel()
+        {
+            return View(CalcService.GetCalcValues());
+        }
+
+        [Route("PassUsingViewBag")]
+        public IActionResult PassUsingViewBag(CalcValues calcValues)
+        {
+            var values = CalcService.GetCalcValues();
+            ViewBag.CalcValues = new CalcValues()
+            {
+                FirstRandValue = values.FirstRandValue,
+                SecondRandValue = values.SecondRandValue,
+                Sum = values.Sum,
+                Difference = values.Difference,
+                Multiplication = values.Multiplication,
+                Division = values.Division
+            };
+            return View();
+        }
+
+        [Route("PassUsingViewData")]
+        public IActionResult PassUsingViewData()
+        {
+            var values = CalcService.GetCalcValues();
+            ViewData["CalcValues"] = new CalcValues()
+            {
+                FirstRandValue = values.FirstRandValue,
+                SecondRandValue = values.SecondRandValue,
+                Sum = values.Sum,
+                Difference = values.Difference,
+                Multiplication = values.Multiplication,
+                Division = values.Division
+            };
+            return View();
+        }
+
+        [Route("AccessServiceDirectly")]
+        public IActionResult AccessServiceDirectly()
+        {
+            return View();
+        }
+    }
+}
